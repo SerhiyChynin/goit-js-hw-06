703481df7a4efc0f411982eb2234b7cae966e492
SerhiiChynin <sergey.chinin@gmail.com>
2023-12-28 14:05:17 +0200

    hw done

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,24 @@
+class StringBuilder{
+    #initialValue;
+
+    constructor(value) {
+        this.#initialValue = value;
+    }
+
+    getValue() {
+        return this.#initialValue;
+    }
+    padStart(str) {
+        this.#initialValue = `${str}${this.#initialValue}`;
+    }
+    padEnd(str) {
+        this.#initialValue += str;
+    }
+    padBoth(str) {
+        this.#initialValue = `${str}${this.#initialValue}${str}`;
+    }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
@@ -6,3 +27,4 @@ builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
+
